1076275e452b79c55f28cce6fd075550b45b7298
sagar-vaghela97 <sagar.vaghela@goapptiv.com>
2022-08-22 11:12:06 +0530

    Add functions in v2 controller

--- src/modules/file/controllers/v2/file.controller.ts
+++ src/modules/file/controllers/v2/file.controller.ts
@@ -1,4 +1,12 @@
-import { Get, Param, Put, Req, UseGuards, Request } from '@nestjs/common';
+import {
+  Get,
+  Param,
+  Put,
+  Req,
+  UseGuards,
+  Request,
+  HttpCode,
+} from '@nestjs/common';
 import { Body, Controller, Post } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
 import { ResponseSuccess } from 'src/shared/interfaces/response-success.interface';
@@ -10,6 +18,14 @@ import { CreateFileVariantDTO } from '../../dto/create-file-variant-dto';
 import { CfFileVariantStatusResponseDTO } from '../../dto/cf-file-variant-response.dto';
 import { FileService } from '../../services/file.service';
 import { CfFileVariantResponseMessage } from '../../interfaces/cf-file-variant-response-message.interface';
+import { ResponseError } from 'src/shared/interfaces/response-error.interface';
+import { RealIP } from 'nestjs-real-ip';
+import { ReadFileBO } from '../../bo/read-file.bo';
+import { BulkReadFileDTO } from '../../dto/bulk-read-file.dto';
+import { BulkReadFileBO } from '../../bo/bulk-read-file.bo';
+import { RegisterFileDTO } from '../../dto/register-file-dto';
+import { RegisterFileBO } from '../../bo/register-file.bo';
+import { ConfirmFileUploadedDTO } from '../../dto/confirm-file-uploaded.dto';
 
 @Controller({
   path: 'files',
@@ -19,11 +35,91 @@ export class FileController {
   constructor(private readonly fileService: FileService) {}
 
   /**
+   * generates a signed url for reading a file.
+   */
+  @Get('uuid/:uuid')
+  @UseGuards(JwtAuthGuard)
+  async getReadUrl(
+    @Param() params: any,
+    @Req() request: Request,
+    @RealIP() ip: string,
+  ): Promise<ResponseSuccess | ResponseError> {
+    const readFile: ReadFileBO = {
+      uuid: params.uuid,
+      ip,
+      userAgent: request.headers['user-agent'],
+    };
+    return RestResponse.success(
+      this.fileService.generateReadSignedUrl(
+        readFile,
+        request['user'].projectId,
+        request['user'].isAdmin,
+      ),
+    );
+  }
+
+  /**
+   * generates a signed url for reading a file in bulk.
+   */
+  @Post('bulk/uuid')
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(200)
+  async getBulkReadUrl(
+    @Body() body: BulkReadFileDTO,
+    @Req() request: Request,
+    @RealIP() ip: string,
+  ): Promise<ResponseSuccess | ResponseError> {
+    const data: BulkReadFileBO = {
+      ...body,
+      ip,
+      userAgent: request.headers['user-agent'],
+    };
+    return RestResponse.success(
+      this.fileService.bulkGenerateReadSignedUrl(
+        data,
+        request['user'].projectId,
+        request['user'].isAdmin,
+      ),
+    );
+  }
+
+  /**
+   * generates a signed url for writing a file.
+   */
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async getUploadUrl(
+    @Body() body: RegisterFileDTO,
+    @Req() request: Request,
+  ): Promise<ResponseSuccess | ResponseError> {
+    const registerFile: RegisterFileBO = {
+      ...body,
+      projectId: request['user'].projectId,
+    };
+    return RestResponse.success(
+      this.fileService.generateUploadSignedUrl(registerFile),
+    );
+  }
+
+  /**
+   * confirms that a file has been uploaded.
+   */
+  @Put('confirm')
+  @UseGuards(JwtAuthGuard)
+  async confirm(
+    @Body() body: ConfirmFileUploadedDTO,
+  ): Promise<ResponseSuccess> {
+    return RestResponse.success(this.fileService.confirmUpload(body.uuid));
+  }
+
+  /**
    * archives a file.
    */
   @Put('archive')
   @UseGuards(JwtAuthGuard)
-  async archive(@Body() body: ArchiveFileDTO): Promise<ResponseSuccess> {
+  async archive(
+    @Body() body: ArchiveFileDTO,
+  ): Promise<ResponseSuccess | ResponseError> {
     const result = await this.fileService.archiveDirectory(body.uuid);
     return RestResponse.success(result);
   }
@@ -36,7 +132,7 @@ export class FileController {
   async createFileVariants(
     @Param() params: any,
     @Req() request: Request,
-  ): Promise<ResponseSuccess> {
+  ): Promise<ResponseSuccess | ResponseError> {
     const response = await this.fileService.generateFileVariantReadSignedUrl(
       params.uuid,
       request['user'].projectId,
@@ -53,7 +149,7 @@ export class FileController {
   async getFileVariantsReadUrl(
     @Body() body: CreateFileVariantDTO,
     @Req() request: Request,
-  ): Promise<ResponseSuccess> {
+  ): Promise<ResponseSuccess | ResponseError> {
     const createVariant: CreateFileVariantBO = { ...body };
     const variants = await this.fileService.createFileVariants(
       createVariant,
@@ -73,7 +169,7 @@ export class FileController {
   @Post('variants/cf-status-response')
   async updateFileVariantStatus(
     @Body() body: CfFileVariantStatusResponseDTO,
-  ): Promise<ResponseSuccess> {
+  ): Promise<ResponseSuccess | ResponseError> {
     const buf = Buffer.from(body.message.data, 'base64');
     const data: CfFileVariantResponseMessage = JSON.parse(buf.toString());
 
